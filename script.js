df9cf374bb6180f21259dc9996b67174c463d2f7
irsyadfata99 <irsyadfata99@gmail.com>
2022-09-05 07:59:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// DOM Selection
+// document.getElementById() -> mengembalikan ID
+// const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = '#cccc';
+// judul.innerHTML = 'aldi';
+
+// document.getElementsByTagName() 
+// -> HTMLCOLLECTION
+// const p = document.getElementsByTagName('p');
+
+// for(let i = 0; i < p.length; i++ ) {
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+// document.getElementsByClassName()
+// -> HTMLCollection
+// const p1 = document.getElementsByClassName('p1');
+// p1[0].innerHTML = 'ini diubah dari javascript';
+
+// document.querySelector() -> Menghasilkan element
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
+
+// const p = document.querySelector('p');
+// p.innerHTML = 'ini ganti pake javascript';
+
+// document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+
+// for( let i = 0; i < p.length; i++ ) {
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// Mengubah node root
+// const sectionB = document.getElementById('b');
+// const p4 = sectionB.querySelector('p');
+// p4.style.backgroundColor = 'orange';
+
+// innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'Irsyad Fata';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = "hello world";
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = "lightblue";
+// judul.style.backgroundColor = "salmon";
+
+// element.setAttribute()
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'irsyad');
+
+// element.classList()
+// const p2 = document.querySelector('.p2')
+
+
